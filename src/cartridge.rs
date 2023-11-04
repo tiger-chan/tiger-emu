1c9a9e2ac45bb3e4316022fcbae00661f99a01da
Anthony Young <anthony.r.young@gmail.com>
2023-11-04 14:16:59 -0600

    Remove previous implementation of nes

--- src/cartridge.rs
+++
@@ -1,9 +0,0 @@
-pub trait Cartridge {
-    fn cpu_read(&self, addr: u16) -> Option<u8>;
-    fn cpu_read_only(&self, addr: u16) -> Option<u8>;
-    fn cpu_write(&mut self, addr: u16, data: u8) -> Option<()>;
-
-    fn ppu_read(&self, addr: u16) -> Option<u8>;
-    fn ppu_read_only(&self, addr: u16) -> Option<u8>;
-    fn ppu_write(&mut self, addr: u16, data: u8) -> Option<()>;
-}
--- src/ppu_bus.rs
+++
@@ -1,5 +0,0 @@
-pub trait PpuBus {
-    fn read(&self, addr: u16) -> u8;
-    fn read_only(&self, addr: u16) -> u8;
-    fn write(&mut self, addr: u16, data: u8);
-}
